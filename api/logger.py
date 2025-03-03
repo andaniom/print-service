94478f5533a3944ca63ac8361cd91964ce6e9c66
andaniom <andaniom@andanioms-MacBook-Pro.local>
2025-03-03 16:02:30 +0700

    update

--- api/logger.py
+++ api/logger.py
@@ -2,5 +2,9 @@ import logging
 
 from api.config import Config
 
-logging.basicConfig(level=Config.LOG_LEVEL)
+logging.basicConfig(
+    filename="backend.log",
+    level=Config.LOG_LEVEL,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 logger = logging.getLogger(__name__)
