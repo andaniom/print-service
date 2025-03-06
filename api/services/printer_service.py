c09f4faa2d8ad551140531899ae59c64e1ca88a6
andaniom <andaniom@andanioms-MacBook-Pro.local>
2025-03-06 14:31:48 +0700

    update

--- api/services/printer_service.py
+++ api/services/printer_service.py
@@ -97,7 +97,7 @@ def print_pdf(pdf_file: str, page_number: int, printer_label: str):
             ]
 
             # Log the command being executed
-            logging.info(f"Executing command: {' '.join(command)}")
+            logging.debug(f"Executing command: {' '.join(command)}")
 
             # Run the command
             result = subprocess.run(command, capture_output=True, text=True)
