700340e3324bab9afcac67c81f37fb52b14aad45
andaniom <andaniom@andanioms-MacBook-Pro.local>
2025-03-07 16:40:18 +0700

    update

--- api/services/queue_service.py
+++ api/services/queue_service.py
@@ -3,29 +3,34 @@ import threading
 
 from api.logger import logger
 from api.services.printer_service import print_pdf
+
 print_queue = queue.Queue()
+
+
+def process_queue_item(item):
+    pdf_file, key, metadata_json = item
+    if pdf_file is None:
+        return
+
+    try:
+        if key:
+            printer_label = key
+            print_pdf(pdf_file, 1, printer_label)
+        elif metadata_json:
+            data = metadata_json['data']
+            for item in data:
+                printer_label = item['printer']
+                print_pdf(pdf_file, item['page'], printer_label)
+        else:
+            logger.error("key empty. Skipping print job.")
+    except Exception as e:
+        logger.error(f"Failed to print {pdf_file}: {e}")
+
 
 def process_queue():
     while True:
-        pdf_file, key, metadata_json = print_queue.get()
-        if pdf_file is None:
-            break
-        if key:
-            try:
-                printer_label = key
-                print_pdf(pdf_file, 1, printer_label)
-            except Exception as e:
-                logger.error(f"Failed to print {pdf_file}: {e}")
-        elif metadata_json:
-            try:
-                data = metadata_json['data']
-                for item in data:
-                    printer_label = item['printer']
-                    print_pdf(pdf_file, item['page'], printer_label)
-            except Exception as e:
-                logger.error(f"Failed to print {pdf_file}: {e}")
-        else:
-            logger.error("key empty. Skipping print job.")
+        item = print_queue.get()
+        process_queue_item(item)
         print_queue.task_done()
 
 
