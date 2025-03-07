ab7ac3ec61374d59342150c17805c8e5641597b6
andaniom <andaniom@andanioms-MacBook-Pro.local>
2025-03-07 14:26:54 +0700

    update

--- api/api.py
+++ api/api.py
@@ -7,7 +7,7 @@ from fastapi.responses import JSONResponse
 
 from api.logger import logger
 from api.services.file_service import save_file
-from api.services.queue_service import e_ticket_print_queue, print_queue
+from api.services.queue_service import e_ticket_print_queue
 
 app = FastAPI()
 
@@ -37,10 +37,10 @@ async def add_to_queue(file: UploadFile = File(...), metadata: str = Form(...)):
 
 @app.post("/print")
 def print_single(file: UploadFile = File(...), key: str = Form(...)):
-    current_time_millis = int(round(time.time() * 1000))
-    filename = key + "_" + str(current_time_millis) + ".pdf"
-    temp_file = save_file(file, filename)
-    print_queue.put((temp_file, key))
+    # current_time_millis = int(round(time.time() * 1000))
+    # filename = key + "_" + str(current_time_millis) + ".pdf"
+    # temp_file = save_file(file, filename)
+    # print_queue.put((temp_file, key))
     return JSONResponse(content={"message": "PDF added to print queue"})
 
 @app.get("/")
@@ -50,7 +50,7 @@ def index():
 @app.on_event("shutdown")
 def shutdown():
     e_ticket_print_queue.put(None)
-    print_queue.put(None)
+    # print_queue.put(None)
     logger.info("Shutting down the application")
 
 if __name__ == "__main__":
--- api/services/queue_service.py
+++ api/services/queue_service.py
@@ -5,7 +5,7 @@ from api.logger import logger
 from api.services.printer_service import print_pdf
 
 e_ticket_print_queue = queue.Queue()
-print_queue = queue.Queue()
+# print_queue = queue.Queue()
 
 
 def process_e_ticket_queue():
@@ -29,20 +29,20 @@ def process_e_ticket_queue():
 threading.Thread(target=process_e_ticket_queue, daemon=True).start()
 
 
-def process_queue():
-    while True:
-        pdf_file, key = print_queue.get()
-        if pdf_file is None:
-            break
-        if key:
-            try:
-                printer_label = key
-                print_pdf(pdf_file, 1, printer_label)
-            except Exception as e:
-                logger.error(f"Failed to print {pdf_file}: {e}")
-        else:
-            logger.error("key empty. Skipping print job.")
-        e_ticket_print_queue.task_done()
-
-
-threading.Thread(target=process_queue, daemon=True).start()
+# def process_queue():
+#     while True:
+#         pdf_file, key = print_queue.get()
+#         if pdf_file is None:
+#             break
+#         if key:
+#             try:
+#                 printer_label = key
+#                 print_pdf(pdf_file, 1, printer_label)
+#             except Exception as e:
+#                 logger.error(f"Failed to print {pdf_file}: {e}")
+#         else:
+#             logger.error("key empty. Skipping print job.")
+#         e_ticket_print_queue.task_done()
+#
+#
+# threading.Thread(target=process_queue, daemon=True).start()
